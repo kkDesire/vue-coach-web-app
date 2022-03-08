0d97b84e26e949c3835f5a1c2574b5a4c3c10dd4
Desire KOUASSI <kouamdesire90@gmail.com>
2022-03-08 08:58:27 +0000

    Working on the Coaches list and list Items

--- src/store/modules/coaches/index.js
+++ src/store/modules/coaches/index.js
@@ -9,12 +9,12 @@ export default {
       coaches: [
         {
           id: "c1",
-          firstName: "Maximilian",
-          lastName: "Schwarzmüller",
+          firstName: "KOUASSI",
+          lastName: "Désiré",
           areas: ["frontend", "backend", "career"],
           description:
-            "I'm Maximilian and I've worked as a freelance web developer for years. Let me help you become a developer as well!",
-          hourlyRate: 30,
+            "I'm Désiré and I've worked as a freelance web developer for years. Let me help you become a developer as well!",
+          hourlyRate: 24,
         },
         {
           id: "c2",
@@ -23,7 +23,7 @@ export default {
           areas: ["frontend", "career"],
           description:
             "I am Julie and as a senior developer in a big tech company, I can help you get your first job or progress in your current role.",
-          hourlyRate: 30,
+          hourlyRate: 26,
         },
       ],
     };
