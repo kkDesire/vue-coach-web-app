9936edc4785fa1f40c3dff1a89721df50ba45707
Desire KOUASSI <kouamdesire90@gmail.com>
2022-03-09 16:19:48 +0000

    The 'Not Found' Page & Summary

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <base-card>
+      <p>Page not found</p>
+      <p>
+        This page could not be found - maybe check out all our
+        <router-link to="/coaches">coaches</router-link>.
+      </p>
+    </base-card>
+  </section>
+</template>
