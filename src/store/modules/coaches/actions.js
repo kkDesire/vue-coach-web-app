7dab6956b6bcacb1447915e065a94e1069469a61
Desire KOUASSI <kouamdesire90@gmail.com>
2022-03-09 10:15:17 +0000

    Sending a PUT Http Request to Store Coach Data

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,13 +1,29 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId,
       firtName: data.first,
       lastName: data.last,
       description: data.desc,
       hourlyRate: data.rate,
       areas: data.areas,
     };
-    context.commit("registerCoach", coachData);
+    const response = await fetch(
+      `https://vue-http-demo-f143c-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      {
+        method: "PUT",
+        body: JSON.stringify(coachData),
+      }
+    );
+    // const responseData = await response.json();
+
+    if (!response.ok) {
+      // error ..
+    }
+
+    context.commit("registerCoach", {
+      ...coachData,
+      id: userId,
+    });
   },
 };
