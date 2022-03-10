06893af4062b3635bd737f22c1b7e547a2ff3463
Desire KOUASSI <kouamdesire90@gmail.com>
2022-03-10 11:40:02 +0000

    Adding a 'Login' Action & Flow

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -61,7 +61,6 @@ export default {
   methods: {
     async submitForm() {
       this.formIsValid = true;
-
       if (
         this.email === "" ||
         !this.email.includes("@") ||
@@ -71,14 +70,15 @@ export default {
         return;
       }
       this.isLoading = true;
+      const actionPayload = {
+        email: this.email,
+        password: this.password,
+      };
       try {
         if (this.mode === "login") {
-          //...
+          await this.$store.dispatch("login", actionPayload);
         } else {
-          await this.$store.dispatch("signup", {
-            email: this.email,
-            password: this.password,
-          });
+          await this.$store.dispatch("signup", actionPayload);
         }
       } catch (err) {
         this.error = err.message || "Failed to authenticate, try later.";
